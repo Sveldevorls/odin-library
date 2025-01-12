d6212109abec3b8d0e9596cc6ab8f1764abc338f
Sveldevorls <64679979+Sveldevorls@users.noreply.github.com>
2025-01-13 01:21:15 +0800

    Add book display and removal logic

--- js.js
+++ js.js
@@ -21,19 +21,67 @@ newBookSubmit.addEventListener("click", (e) => {
                          formData.get("author"),
                          parseInt(formData.get("pages"), 10),
                          formData.get("read") == "yes" ? true : false);
+        renderBooks();
     }
 })
 
 const myLibrary = [];
+let bookID = 0;
 
 // constructor
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, id) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = id;
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    myLibrary.push(new Book(title, author, pages, read));
+    myLibrary.push(new Book(title, author, pages, read, bookID));
+    bookID++;
+}
+
+
+const booksDisplay = document.body.querySelector(".display");
+
+function renderBooks() {
+    booksDisplay.replaceChildren();
+
+    for (let book of myLibrary) {
+        let card = document.createElement("div");
+        card.className = "card";
+        card.id = book.id;
+        let title = document.createElement("p");
+        title.innerText = book.title;
+        title.className = "title"
+        let author = document.createElement("p");
+        author.innerText = `by ${book.author}`;
+        let pages = document.createElement("p");
+        pages.innerText = book.pages == 1? `${book.pages} page` : `${book.pages} pages`;
+        let read = document.createElement("p");
+        read.innerText = book.read ? "Read ✔" : "Not read ✘";
+        read.className = book.read ? "read yes" : "read no";
+        let readToggle = document.createElement("button");
+        readToggle.innerText = book.read ? "Mark as not read" : "Mark as read";
+        readToggle.className = book.read ? "read no" : "read yes";
+        let removeButton = document.createElement("button");
+        removeButton.innerText = "Remove this book";
+        removeButton.addEventListener("click", (e) => {
+            let removalID = e.target.parentNode.id;
+            for (let i = 0; i < myLibrary.length; i++) {
+                if (myLibrary[i].id = removalID) {
+                    myLibrary.splice(i, 1);
+                } 
+            }
+            e.target.parentNode.remove();
+        })
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(read);
+        card.appendChild(readToggle);
+        card.appendChild(removeButton);
+        booksDisplay.appendChild(card);
+    }
 }
