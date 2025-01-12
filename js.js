23afa74b98bc610185112048301a7648a0ad15e2
Sveldevorls <64679979+Sveldevorls@users.noreply.github.com>
2025-01-12 21:58:55 +0800

    Add new book function

--- js.js
+++ js.js
@@ -1,11 +1,39 @@
-const newBookDialog = document.body.querySelector("dialog")
-const newBookDialogAdd = document.body.querySelector("#newbook-add")
-const newBookDialogCancel = document.body.querySelector("#newbook-cancel")
+const newBookDialog = document.body.querySelector("dialog");
+const newBookAdd = document.body.querySelector("#newbook-add");
+const newBookCancel = document.body.querySelector("#newbook-cancel");
+const newBookSubmit = document.body.querySelector("#newbook-submit");
+const newBookForm = document.forms["newbook-form"]
 
-newBookDialogAdd.addEventListener("click", () => {
+newBookAdd.addEventListener("click", () => {
     newBookDialog.showModal();
 })
 
-newBookDialogCancel.addEventListener("click", () => {
+newBookCancel.addEventListener("click", () => {
     newBookDialog.close();
 })
+
+newBookSubmit.addEventListener("click", (e) => {
+    let formIsValid = newBookForm.checkValidity();
+    if (formIsValid) {
+        e.preventDefault();
+        let formData = new FormData(newBookForm);
+        addBookToLibrary(formData.get("title"),
+                         formData.get("author"),
+                         parseInt(formData.get("pages"), 10),
+                         formData.get("read") == "yes" ? true : false);
+    }
+})
+
+const myLibrary = [];
+
+// constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    myLibrary.push(new Book(title, author, pages, read));
+}
