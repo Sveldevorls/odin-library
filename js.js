762b07ab39aba35359fe5875d91eb98da76b1ad7
Sveldevorls <64679979+Sveldevorls@users.noreply.github.com>
2025-01-13 19:34:07 +0800

    Clean up js logic

--- js.js
+++ js.js
@@ -4,6 +4,8 @@ const newBookCancel = document.body.querySelector("#newbook-cancel");
 const newBookSubmit = document.body.querySelector("#newbook-submit");
 const newBookForm = document.forms["newbook-form"]
 
+const booksDisplay = document.body.querySelector(".display");
+
 const myLibrary = [];
 let bookID = 0;
 
@@ -21,10 +23,10 @@ newBookSubmit.addEventListener("click", (e) => {
         e.preventDefault();
         let formData = new FormData(newBookForm);
         let thisBook = addBookToLibrary(formData.get("title"),
-                         formData.get("author"),
-                         parseInt(formData.get("pages"), 10),
-                         formData.get("read") == "yes" ? true : false,
-                         parseInt(bookID, 10));
+                                        formData.get("author"),
+                                        parseInt(formData.get("pages"), 10),
+                                        formData.get("read") == "yes" ? true : false,
+                                        parseInt(bookID, 10));
         renderBook(thisBook);
         bookID += 1;
     }
@@ -45,26 +47,27 @@ function addBookToLibrary(title, author, pages, read, bookID) {
     return newBook;
 }
 
-
-const booksDisplay = document.body.querySelector(".display");
+function newElement(type, ...attributesArr) {
+    let thisElement = document.createElement(type);
+    if (attributesArr) {
+        for ([attribute, value] of attributesArr) {
+            thisElement[attribute] = value;
+        }
+    }
+    return thisElement
+}
 
 function renderBook(book) {
-    let card = document.createElement("div");
-    card.className = "card";
-    card.id = book.id;
-    let title = document.createElement("p");
-    title.innerText = book.title;
-    title.className = "title"
-    let author = document.createElement("p");
-    author.innerText = `by ${book.author}`;
-    let pages = document.createElement("p");
-    pages.innerText = book.pages == 1? `${book.pages} page` : `${book.pages} pages`;
-    let read = document.createElement("p");
-    read.innerText = book.read ? "Read ✔" : "Not read ✘";
-    read.className = book.read ? "read yes" : "read no";
-    let readToggle = document.createElement("button");
-    readToggle.innerText = book.read ? "Mark as not read" : "Mark as read";
-    readToggle.className = book.read ? "read no" : "read yes";
+    let card = newElement("div", ["className", "card"], ["id", book.id]);
+    let title = newElement("p", ["className", "title"], ["innerText", book.title]);
+    let author = newElement("p", ["innerText", `by ${book.author}`]);
+    let pages = newElement("p", ["innerText", book.pages == 1 ? `${book.pages} page` : `${book.pages} pages`]);
+    let read = newElement("p", ["className", book.read ? "read yes" : "read no"],
+                                ["innerText", book.read ? "Read ✔" : "Not read ✘"]);
+    let readToggle = newElement("button", ["className", book.read ? "read no" : "read yes"],
+                                           ["innerText", book.read ? "Mark as not read" : "Mark as read"]);
+    let removeButton = newElement("button", ["innerText", "Remove this book"])
+    
     readToggle.addEventListener("click", (e) => {
         let thisButton = e.target;
         let thisBookID = thisButton.parentNode.id;
@@ -78,13 +81,11 @@ function renderBook(book) {
         read.innerText = book.read ? "Read ✔" : "Not read ✘";
         read.className = book.read ? "read yes" : "read no";
     })
-    let removeButton = document.createElement("button");
-    let targetID = 0;
-    removeButton.innerText = "Remove this book";
+
     removeButton.addEventListener("click", (e) => {
         let removalID = e.target.parentNode.id;
+        let targetID = 0;
         for (let i = 0; i < myLibrary.length; i++) {
-            console.log(myLibrary[i])
             if (myLibrary[i].id == removalID) {
                 targetID = i;
             } 
@@ -92,11 +93,10 @@ function renderBook(book) {
         myLibrary.splice(targetID, 1)
         e.target.parentNode.remove();
     })
-    card.appendChild(title);
-    card.appendChild(author);
-    card.appendChild(pages);
-    card.appendChild(read);
-    card.appendChild(readToggle);
-    card.appendChild(removeButton);
+
+    for (childElement of [title, author, pages, read, readToggle, removeButton]) {
+        card.appendChild(childElement);
+    }
+
     booksDisplay.appendChild(card);
 }
